997501b224a4cfd323cdbe33f54b9012dc06ae1e
trace1729 <1106029170@qq.com>
2022-01-26 20:49:01 +0800

    add cat funtionaily

--- greet.py
+++ greet.py
@@ -3,8 +3,14 @@ import sys
 def default():
     print('hello world.')
 
+def cat( ):
+    print('Meeo.')
+
 def main():
-    default()
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
